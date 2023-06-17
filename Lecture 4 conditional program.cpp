e9d966d96dca752f5fcc49d4d82352327108f798
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-17 14:46:41 +0530

    Add files via upload

---
+++ Lecture 4 conditional program.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int a,b;
+	int c,d;
+	cout<<"Input 4  integers a,b,c and d"<<endl  ;
+	
+	cin>>a;
+	cin>>b;
+	cin>>c;
+	cin>>d; 
+	//statement  :a>b
+	cout<<(a>b && c>d);
+	//cout<<(a>b || c>d);
+	//C++ interpets the true  as 1 
+	//C++ interpets false as 0 
+}
+
+	//execute  AND and OR statements and print out the boolean 
+
