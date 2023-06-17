22395c45b62e692f009805c69ce4aed49943e442
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-17 14:51:24 +0530

    Add files via upload

---
+++ Lecture 3- variables.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+//declare 2 variables and then assign them values of 3,5 and then add them 
+	
+	int a=3;;
+	int b=5;
+	
+	
+	//a=3;
+	//b=5;
+	
+	//int c;
+	
+
+	
+	cout<<a+b;
+	cout<<"a+b";
+}
