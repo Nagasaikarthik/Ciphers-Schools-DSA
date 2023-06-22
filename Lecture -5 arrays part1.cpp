32e63a5948b0a2e6c61468dd7879fd54cc57b2d7
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-22 21:57:50 +0530

    Add files via upload

---
+++ Lecture -5 arrays part1.cpp
@@ -0,0 +1,20 @@
+//intialization of arrays 
+//reading the elements of an array 
+//print 10 numbers consecutively 
+//printing the address of the elements in a array
+//arrays : array is an datastruture which can represent a collection of 
+// data items which have the same data type(float,int ,char) 
+#include<iostream>
+ using namespace std;
+ 
+ int main()
+ {
+ 	int a,b,c,d,e;
+ 	cout<<"Give 5 integers as input "<<endl;
+ 	cin>>a>>b>>c>>d>>e;
+ 	
+ 	cout<<"the integer in reverse order are : "<<e<<d<<c<<b<<a;
+ 	
+ 	return 0;
+ 	
+ }
