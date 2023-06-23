87bd3cf9a24c1583f7d6ea49d61decc5a7753123
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-23 15:30:09 +0530

    Add files via upload

---
+++ Lecture 6 loops part 1.cpp
@@ -0,0 +1,54 @@
+//loops in c++
+//what are the loops 
+// what is the importance of loops
+// for and while loops 
+//break statement 
+//--- a loop is something which repeats itself 
+#include<iostream>
+using namespace std;
+int main()
+{
+	/*int i,j,k;
+	i=0;
+	j=0;
+	k=0;
+	
+	cout<<i<<" "<<j<<" "<<k<<endl;
+	j=i++;
+	cout<<i<<" "<<j<<" "<<k<<endl;
+	k=++i;
+	cout<<i<<" "<<j<<" "<<k<<endl;
+	cout<<++i<<endl;
+	cout<<i;
+	*/
+	
+	int i;
+	for(i=1;  ;i++)// called the update which happens after the interrput t
+	{
+	 cout<<i<<"" ;//block which gets executed every time 
+	 
+	}
+	
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// for loops 
+//for(intilization;condition;update)
+//for{code to be excuted}
