b9e698070d71f411cb91645966eb66df9305f750
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-29 12:01:51 +0530

    Add files via upload

---
+++ Lecture-9 functions part3.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int compare (int a,int b)//compare to the  2 integers and returns the greater value 
+{
+	int c;
+	if (a==5)
+	
+	{
+		return 1;
+	}
+	return 0;	
+	
+}
+
