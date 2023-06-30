dc4e4def974e395bfe42af32b7ba8dd3c6df409b
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-06-30 15:47:33 +0530

    Add files via upload

---
+++ Lecture-11 address part2.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	long long int a;
+	int b;
+	float c;
+	
+	//sizeof(a) ---->function 
+	cout<<"The size of int is :"<<sizeof(a)<<"the size of long long int is:"<<sizeof(a);
+	cout<<"the address of c is"<<&a<<"the address of b is"<<&b;
+	
+	return 0;
+	
+}
