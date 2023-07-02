ec2668e5676998f1b399cf0a993dc997ba8957f1
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-07-02 07:17:24 +0530

    Add files via upload

---
+++ Lecture-14 parameter passing.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+using namespace std;
+
+//void swap(int*x,int*y)
+//{
+	/*int c;//x and y are pointers to a,b 
+	c=*x;
+	*x=*y;
+	*y=c;
+	cout<<"the swapping is complete "<<endl;*/
+	
+//}
+//int main()
+//{
+	/*int a,b;
+	a=3;
+	b=4;
+	swap(&a,&b);
+	cout<<"the values of a and b after the swap are:"<<a<<" "<<b<<endl;
+	return 0;
+	*/
+//}
+int main()
+{
+	int a[10];
+	cout<<"the values for comparision :"<<endl;
+	cout<<"the value  of a is :"<<endl;
+	cout<<"the values of address a[0] is :"<<&a[0]<<endl;
+	cout<<"the values of address a[1] is :"<<&a[1]<<endl;
+}
+
