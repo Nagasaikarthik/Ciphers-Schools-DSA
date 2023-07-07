284722892624e46b7cb02baa5be999088498153d
G Saikarthik <99310584+Nagasaikarthik@users.noreply.github.com>
2023-07-07 15:47:20 +0530

    Add files via upload

---
+++ Lecture-34 tree part1.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+using namespace std;
+
+class Node
+{
+	public:
+		int data;
+		//Node *next1;
+		//Node *next2;
+		//Node *next3;
+		Node *left;
+		Node *right;
+		
+		//note : since each node has the address ti the node corresponding to the root,we are able to 
+		//access the linked list ;
+		
+		
+		
+};
+
+int main()
+{
+	string CEO;
+	string PA,CFO;
+	
+};
